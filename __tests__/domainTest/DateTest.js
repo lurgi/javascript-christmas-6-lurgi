953cbede320f787ee0f94fa08af69e99de2922e0
lurgi <wahoo9040@naver.com>
2023-11-11 12:14:52 +0900

    test(DateTest): 유효성 검사

---
+++ __tests__/domainTest/DateTest.js
@@ -0,0 +1,13 @@
+import ReserveDate from '../../src/Model/ReserveDate';
+
+describe('날짜 모델 유효성', () => {
+  const ERROR_CASE = [[0], [32], [NaN], [3224], [-1]];
+  test.each(ERROR_CASE)('모델 생성 에러', (menu) => {
+    expect(() => new ReserveDate(menu)).toThrow('[ERROR]');
+  });
+
+  const PASS_CASE = Array.from({ length: 31 }, (_, index) => index + 1);
+  test.each(PASS_CASE)('모델 생성 성공', (menu) => {
+    expect(new ReserveDate(menu)).toBeTruthy();
+  });
+});
