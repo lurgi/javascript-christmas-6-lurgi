1e4e50987285f80d91585e9c68b0418d2663b8cf
lurgi <wahoo9040@naver.com>
2023-11-11 12:00:57 +0900

    test(MenuTest): 음료만 주문 확인

--- __tests__/domainTest/MenuTest.js
+++ __tests__/domainTest/MenuTest.js
@@ -1,6 +1,6 @@
 import ReserveMenu from '../../src/Model/ReserveMenu';
 
-describe('메뉴 모델 테스트', () => {
+describe('메뉴 모델 유효성', () => {
   const ERROR_CASE = [
     [['양송이파스타-1']],
     [['양송이수프-2', '크리스마스파스타-0']],
@@ -17,7 +17,17 @@ describe('메뉴 모델 테스트', () => {
     [['타파스-1', '제로콜라-1']],
   ];
   test.each(PASS_CASE)('모델 생성 성공', (menu) => {
-    const MENU = new ReserveMenu(menu);
-    expect(MENU).toBeTruthy();
+    expect(new ReserveMenu(menu)).toBeTruthy();
+  });
+});
+
+describe('음료만 주문 여부', () => {
+  const ERROR_CASE = [
+    [['제로콜라-4']],
+    [['레드와인-1']],
+    [['제로콜라-1', '레드와인-1', '샴페인-1']],
+  ];
+  test.each(ERROR_CASE)('음료만 주문 실패', (menu) => {
+    expect(() => new ReserveMenu(menu)).toThrow('[ERROR]');
   });
 });
