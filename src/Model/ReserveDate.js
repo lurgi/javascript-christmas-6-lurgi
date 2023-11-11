8c6a6e42c41028edc62950e8e4d6e1f6542f9f25
lurgi <wahoo9040@naver.com>
2023-11-11 12:15:30 +0900

    feat(ReserveDate): 유효성 검사

--- src/Model/ReserveDate.js
+++ src/Model/ReserveDate.js
@@ -1,6 +1,16 @@
 class ReserveDate {
   constructor(number) {
+    this.validate(number);
     this.reserveDate = number;
+  }
+
+  validate(number) {
+    if (number < 1 || number > 31) {
+      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    }
+    if (Number.isNaN(number)) {
+      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    }
   }
 
   applyDateDiscount() {}
