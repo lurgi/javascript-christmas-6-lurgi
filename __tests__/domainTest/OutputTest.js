5e6cd1bba89e0df9a8dd3b8324158cce040ca418
lurgi <wahoo9040@naver.com>
2023-11-11 17:41:31 +0900

    test(OutputTest): 혜택 출력

--- __tests__/domainTest/OutputTest.js
+++ __tests__/domainTest/OutputTest.js
@@ -43,4 +43,32 @@ describe('출력 테스트', () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
+
+  test('혜택 출력', () => {
+    const logSpy = getLogSpy();
+    const MENU_IFNO = {
+      menuInfo: { 티본스테이크: 1, 바비큐립: 1, 초코케이크: 2, 제로콜라: 1 },
+      amountBeforeDiscount: 142000,
+      dDayDiscount: 1200,
+      weekDayOrEnd: { isWeekEnd: false, weekDiscount: 4046 },
+      isStar: true,
+    };
+    const OUTPUTS = [
+      '<혜택 내역>',
+      '크리스마스 디데이 할인: -1,200원',
+      '평일 할인: -4,046원',
+      '특별 할인: -1,000원',
+      '증정 이벤트: -25,000원',
+      '<총혜택 금액>',
+      '-31,246원',
+      '<할인 후 예상 결제 금액>',
+      '135,754원',
+      '<12월 이벤트 배지>',
+      '산타',
+    ];
+    OutputView.printBenefit(MENU_IFNO);
+    OUTPUTS.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 });
