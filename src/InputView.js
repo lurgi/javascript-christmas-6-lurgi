3056285eedd6c659c4f7479a0afa86cfdc6fa8df
lurgi <wahoo9040@naver.com>
2023-11-14 08:47:46 +0900

    refactor(InputView): 메서드, 상수 분리
    refactor(Input)
    
    fix(InputView)
    
    chore(InputView)

--- src/InputView.js
+++ src/InputView.js
@@ -1,26 +1,30 @@
 import { Console } from '@woowacourse/mission-utils';
 
+const DATE_QUESTION = '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n';
+const MENU_QUESTION = `주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n`;
+
+const DIVIDE_SYMBOL = ',';
+
+const ERROR_MESSAGE = '[ERROR] 입력하지 않으셨습니다.';
+
 const InputView = {
   async readDate() {
-    const input = await Console.readLineAsync(
-      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
-    );
-    this.validate(input);
+    const input = await Console.readLineAsync(DATE_QUESTION);
+    this.validInput(input);
+
     return Number(input);
   },
 
   async readMenu() {
-    const input = await Console.readLineAsync(
-      `주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)`,
-    );
-    this.validate(input);
-    const MENU_INFO = input.split(',');
-    return MENU_INFO;
+    const input = await Console.readLineAsync(MENU_QUESTION);
+    this.validInput(input);
+
+    return input.split(DIVIDE_SYMBOL);
   },
 
-  validate(input) {
-    if (input.trim() === '') {
-      throw new Error('[ERROR]');
+  validInput(input) {
+    if (!input.trim()) {
+      throw new Error(ERROR_MESSAGE);
     }
   },
 };
