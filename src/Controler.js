176f97f3eb2096a427132afe31b9ebc55bf83b28
lurgi <wahoo9040@naver.com>
2023-11-14 18:13:18 +0900

    feat(Controler): 만원 이상 할인 적용
    fix
    
    chore(Controler)

--- src/Controler.js
+++ src/Controler.js
@@ -1,3 +1,5 @@
+const DISCOUNT_BASE_AMOUNT = 10000;
+
 class Controler {
   setReserveDate(dateClass) {
     this.reserveDate = dateClass;
@@ -9,24 +11,36 @@ class Controler {
 
   getPrintInfo() {
     const AMOUNT_BEFORE_DISCOUNT = this.reserveMenu.getAmount();
-    const D_DAY_DISCOUNT = this.reserveDate.dDayDiscount();
-    const [IS_WEEKEND, WEEK_DISCOUNT] = this.#getWeekDiscount();
-    const IS_STAR = this.reserveDate.isStarDay();
+
     const MENU_INFO = this.reserveMenu.getMenuInfo();
+    const { dDayDiscount, weekDayOrEnd, isStar } = this.#getDiscountInfo(AMOUNT_BEFORE_DISCOUNT);
 
     return {
       menuInfo: MENU_INFO,
       amountBeforeDiscount: AMOUNT_BEFORE_DISCOUNT,
+      dDayDiscount,
+      weekDayOrEnd,
+      isStar,
+    };
+  }
+
+  #getDiscountInfo(AMOUNT_BEFORE_DISCOUNT) {
+    const isValidDiscount = AMOUNT_BEFORE_DISCOUNT >= DISCOUNT_BASE_AMOUNT;
+    const D_DAY_DISCOUNT = isValidDiscount ? this.reserveDate.dDayDiscount() : 0;
+    const [IS_WEEKEND, WEEK_DISCOUNT] = this.#getWeekDiscount();
+    const IS_STAR = isValidDiscount && this.reserveDate.isStarDay();
+
+    return {
       dDayDiscount: D_DAY_DISCOUNT,
-      weekDayOrEnd: { isWeekEnd: IS_WEEKEND, weekDiscount: WEEK_DISCOUNT },
+      weekDayOrEnd: { isWeekEnd: IS_WEEKEND, weekDiscount: isValidDiscount ? WEEK_DISCOUNT : 0 },
       isStar: IS_STAR,
     };
   }
 
   #getWeekDiscount() {
     const IS_WEEKEND = this.reserveDate.isWeekEnd();
-
     let weekDiscount = 0;
+
     if (IS_WEEKEND) {
       weekDiscount = this.reserveMenu.applyWeekEndDiscount();
     }
