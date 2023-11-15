aaffb21b0e2c5f9603a08bb893299ead919d7d81
lurgi <wahoo9040@naver.com>
2023-11-15 09:49:02 +0900

    test(OutputTest): 테스트 추가

--- __tests__/domainTest/OutputTest.js
+++ __tests__/domainTest/OutputTest.js
@@ -10,62 +10,97 @@ const getLogSpy = () => {
 };
 
 describe('출력 테스트', () => {
-  test('주문 메뉴 출력', () => {
-    const MENU_INFO = {
-      티본스테이크: 1,
-      바비큐립: 1,
-      초코케이크: 2,
-      제로콜라: 1,
-    };
-    const OUTPUTS = [
-      '<주문 메뉴>',
-      '티본스테이크 1개',
-      '바비큐립 1개',
-      '초코케이크 2개',
-      '제로콜라 1개',
-    ];
-    const logSpy = getLogSpy();
-    OutputView.printMenu(MENU_INFO);
-    OUTPUTS.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-    });
+  let logSpy;
+  beforeEach(() => {
+    logSpy = getLogSpy();
   });
 
-  test('할인 전, 증정 출력', () => {
-    const logSpy = getLogSpy();
-    const OUTPUTS = ['<할인 전 총주문 금액>', '142,000원', '<증정 메뉴>', '샴페인 1개'];
-    OutputView.printAmount(142000);
-    OUTPUTS.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-    });
+  const MENU_PRINT_CASES = [
+    [
+      {
+        티본스테이크: 1,
+        바비큐립: 1,
+        초코케이크: 2,
+        제로콜라: 1,
+      },
+      ['\n<주문 메뉴>', '티본스테이크 1개', '바비큐립 1개', '초코케이크 2개', '제로콜라 1개'],
+    ],
+    [
+      {
+        양송이수프: 1,
+        바비큐립: 4,
+        아이스트림: 4,
+      },
+      ['\n<주문 메뉴>', '양송이수프 1개', '바비큐립 4개', '아이스트림 4개'],
+    ],
+  ];
+  test.each(MENU_PRINT_CASES)('주문 메뉴 출력', (menuInfo, outputs) => {
+    OutputView.printMenu(menuInfo);
+    expect(logSpy.mock.calls.map(([output]) => output)).toEqual(outputs);
   });
 
-  test('혜택 출력', () => {
-    const logSpy = getLogSpy();
-    const MENU_INFO = {
-      menuInfo: { 티본스테이크: 1, 바비큐립: 1, 초코케이크: 2, 제로콜라: 1 },
-      amountBeforeDiscount: 142000,
-      dDayDiscount: 1200,
-      weekDayOrEnd: { isWeekEnd: false, weekDiscount: 4046 },
-      isStar: true,
-    };
-    const OUTPUTS = [
-      '<혜택 내역>',
-      '크리스마스 디데이 할인: -1,200원',
-      '평일 할인: -4,046원',
-      '특별 할인: -1,000원',
-      '증정 이벤트: -25,000원',
-      '<총혜택 금액>',
-      '-31,246원',
-      '<할인 후 예상 결제 금액>',
-      '135,754원',
-      '<12월 이벤트 배지>',
-      '산타',
-    ];
-    OutputView.setPrintInfo({ MENU_INFO, DATE_INFO: 3 });
+  const PRESENT_CASE = [
+    [142000, ['\n<할인 전 총주문 금액>', '142,000원', '\n<증정 메뉴>', '샴페인 1개']],
+    [120000, ['\n<할인 전 총주문 금액>', '120,000원', '\n<증정 메뉴>', '샴페인 1개']],
+    [119000, ['\n<할인 전 총주문 금액>', '119,000원', '\n<증정 메뉴>', '없음']],
+  ];
+  test.each(PRESENT_CASE)('할인 전, 증정 출력', (number, outputs) => {
+    OutputView.printAmount(number);
+    expect(logSpy.mock.calls.map(([output]) => output)).toEqual(outputs);
+  });
+
+  const BENEFIT_CASE = [
+    [
+      {
+        MENU_INFO: {
+          menuInfo: { 티본스테이크: 1, 바비큐립: 1, 초코케이크: 2, 제로콜라: 1 },
+          amountBeforeDiscount: 142000,
+          dDayDiscount: 1200,
+          weekDayOrEnd: { isWeekEnd: false, weekDiscount: 4046 },
+          isStar: true,
+        },
+        DATE_INFO: 3,
+      },
+      [
+        '\n<혜택 내역>',
+        '크리스마스 디데이 할인: -1,200원',
+        '평일 할인: -4,046원',
+        '특별 할인: -1,000원',
+        '증정 이벤트: -25,000원',
+        '\n<총혜택 금액>',
+        '-31,246원',
+        '\n<할인 후 예상 결제 금액>',
+        '135,754원',
+        '\n<12월 이벤트 배지>',
+        '산타',
+      ],
+    ],
+    [
+      {
+        MENU_INFO: {
+          menuInfo: { 타파스: 1, 제로콜라: 1 },
+          amountBeforeDiscount: 8500,
+          dDayDiscount: 0,
+          weekDayOrEnd: { isWeekEnd: false, weekDiscount: 0 },
+          isStar: false,
+        },
+        DATE_INFO: 3,
+      },
+      [
+        '\n<혜택 내역>',
+        '없음',
+        '\n<총혜택 금액>',
+        '0원',
+        '\n<할인 후 예상 결제 금액>',
+        '8,500원',
+        '\n<12월 이벤트 배지>',
+        '없음',
+      ],
+    ],
+  ];
+  test.each(BENEFIT_CASE)('혜택 출력', (printInfo, outputs) => {
+    OutputView.setPrintInfo(printInfo);
     OutputView.printBenefit();
-    OUTPUTS.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-    });
+    expect(logSpy.mock.calls.map(([output]) => output)).toEqual(outputs);
   });
 });
