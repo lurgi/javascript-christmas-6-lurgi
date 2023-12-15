22a6fe85549664542fb45ce8c1693254af85927a
lurgi <wahoo9040@naver.com>
2023-12-15 12:01:33 +0900

    docs(*): 기능 구현 목록 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 class App {
-  async run() {}
+  async run() {
+    // TODO 시작 출력
+    // TODO 날짜 입력
+    // TODO 메뉴 입력
+    // TODO 미리보기 문구 출력
+    // TODO 주문 메뉴 출력
+    // TODO 혜택 내역 출력
+    // TODO 총혜택 금액 출력
+    // TODO 할인 후 예상 결제 금액 출력
+    // TODO 뱃지 출력
+  }
 }
 
 export default App;
